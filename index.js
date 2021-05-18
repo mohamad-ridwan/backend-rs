fae64721cb524e292ab18acad16062def8cdc8c6
mohamad-ridwan <mr643062@gmail.com>
2021-05-18 14:02:27 +0700

    upload procfile heroku

--- index.js
+++ index.js
@@ -93,12 +93,12 @@ app.use((error, req, res, next) => {
     res.status(status).json({ message: message, data: data });
 })
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "build", "index.html"))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static("build"));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "build", "index.html"))
+//     })
+// }
 
 const PORT = process.env.PORT || 6200
 
